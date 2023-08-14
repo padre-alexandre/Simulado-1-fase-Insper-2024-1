22c6ba5b9960ece2bd048cb1d493ff27de7931f8
carol-zilker <carolinezilker@gmail.com>
2023-08-14 17:16:33 -0300

    v2

--- simulado_jazz.py
+++ simulado_jazz.py
@@ -351,7 +351,7 @@ if login_aluno != '':
     
 
     base_alunos_fizeram_aux = base_alunos_fizeram.drop(columns = ['Nome da avaliação','Resposta do aluno(a)','Gabarito','Certo ou errado','Assunto'])
-
+    st.dataframe(base_alunos_fizeram_aux)
     resultados_gerais_disciplina = base_alunos_fizeram_aux.groupby(['Turma','Login do aluno(a)','Nome do aluno(a)','Disciplina']).sum().reset_index()
     
     resultados_gerais_disciplina2 = resultados_gerais_disciplina.drop(columns = ['Número da questão'])
@@ -359,7 +359,7 @@ if login_aluno != '':
     resultados_gerais_disciplina3 = resultados_gerais_disciplina2.sort_values(by = 'Nota na questão', ascending = False).reset_index(drop = True)
     
     resultados_gerais_disciplina3['Nota na questão'] = 1000*resultados_gerais_disciplina3['Nota na questão']/resultados_gerais_disciplina3['Valor da questão']
-    st.dataframe(resultados_gerais_disciplina3)
+    
     resultados_gerais_disciplina3_aux = resultados_gerais_disciplina3.drop(columns = ['Turma','Login do aluno(a)','Nome do aluno(a)'])
     
     resultados_gerais_disciplina4 = resultados_gerais_disciplina3_aux.groupby(['Disciplina']).mean().reset_index()
